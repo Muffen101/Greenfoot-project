82faff52317f9513c831886648cc06d6e78ffb64
Muffen101 <steffanlindhard@gmail.com>
2026-09-15 09:35:40 +0200

    Added gif image as intro

--- Greenfoot/Ping/Ping/IntroWorld.java
+++ Greenfoot/Ping/Ping/IntroWorld.java
@@ -10,8 +10,7 @@ public class IntroWorld extends World
 {
     private static final int WORLD_WIDTH = 500;
     private static final int WORLD_HEIGHT = 700;
-    
-    
+    private GifImage introGif = new GifImage("ChumBucketPlay.gif");
     /**
      * Constructor for objects of class IntroWorld.
      */
@@ -19,13 +18,15 @@ public class IntroWorld extends World
     {
         super(WORLD_WIDTH, WORLD_HEIGHT, 1); 
         
-        GreenfootImage background = getBackground();
-        background.setColor(Color.BLACK);
-        background.drawString("Intro world. Hit <enter> to start game...", WORLD_WIDTH / 2 - 100, WORLD_HEIGHT / 2);
+        //GreenfootImage background = getBackground();
+        //background.setColor(Color.BLACK);
+        //background.drawString("Intro world. Hit <enter> to start game...", WORLD_WIDTH / 2 - 100, WORLD_HEIGHT / 2);
     }
     
     public void act()
     {
+        setBackground(introGif.getCurrentImage());
+        
         String key = Greenfoot.getKey();
         if (key != null && key.equals("enter"))
         {
